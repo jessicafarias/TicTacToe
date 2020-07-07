25ff03ce8a6d000f90e6341177e2c9114d37c589
jessica <jessica.farias.rosado@gmail.com>
2020-07-07 11:01:39 -0500

    Adding user_interface

---
+++ bin/main.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+puts "Welcome to TicTacToe\n\n"
+puts "Who is the first player?"
+player1= gets.chomp
+#save the name of the player 1
+puts "#{player1} your symbol is O \n\n"
+#assign a simbol to player 1
+puts "Who is the second player?"
+player2 =gets.chomp
+#save the name of the player 2
+puts "#{player2} your symbol is X \n\n"
+#assign a simbol to player 2
+
+puts "a | b | c"
+puts "d | e | f"
+puts "g | h | i"
+
+
+puts "Player1 choose a position \n\n"
+input1 = gets.chomp
+#We override and display the board
+#Verify win conditions
+puts "Player2 choose a position \n\n"
+input = gets.chomp
+#We override and display the board
+#Verify win conditions
+
