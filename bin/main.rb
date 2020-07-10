993f2245c18438e5d612f0dfe10a9976702b390e
jessica <jessica.farias.rosado@gmail.com>
2020-07-10 11:00:34 -0500

    Fix pull request issues
    Printing in main.rb
    Removed false printed on the board
    Adding "?" convention to methods
    Change while to until

--- bin/main.rb
+++ bin/main.rb
@@ -7,53 +7,57 @@ game = GameCondition.new
 puts 'Welcome to TicTacToe'
 puts 'Who is the first player?'
 player1 = Players.new(gets.chomp, 'X')
-game.player_welcome(player1.name, player1.symbol)
+puts "You're welcome" + ' ' + player1.name + ' Your symbol is ' + player1.symbol
 
 puts 'Who is the second player?'
 player2 = Players.new(gets.chomp, 'O')
-game.player_welcome(player2.name, player2.symbol)
+puts "You're welcome" + ' ' + player2.name + ' Your symbol is ' + player2.symbol
 
 rematch = true
 while rematch == true
 
-  while game.check_win == false
+  until game.check_win?
     loop do
-      game.printboard
-      puts "#{player1.name} choose a position \n\n"
+      print game.printboard
+      print "#{player1.name} choose a position \n\n"
       input1 = gets.chomp
       if GameCondition.convertsymbol(input1) != 'No Valid'
-        override = game.position_override(GameCondition.convertsymbol(input1), 'X')
+        override = game.position_override?(GameCondition.convertsymbol(input1), 'X')
       else
-        puts 'Invalid character!'
+        puts 'Position no available'
       end
       break if override
     end
-
-    break if game.check_win
+    break if game.check_win?
 
     loop do
-      game.printboard
+      print game.printboard
       puts "#{player2.name} choose a position \n\n"
       input2 = gets.chomp
       if GameCondition.convertsymbol(input2) != 'No Valid'
-        override = game.position_override(GameCondition.convertsymbol(input2), 'O')
+        override = game.position_override?(GameCondition.convertsymbol(input2), 'O')
       else
         puts 'Invalid character!'
       end
       break if override
     end
-    p game.check_win
-
   end
-  game.printboard
+  print game.printboard
 
-  var = game.who_is_winning(game.check_win, player1.name, player2.name)
-  player1.winner_counter if player1.name == var
-  player2.winner_counter if player2.name == var
-  puts "\n RESULTS= P1=" + player1.count.to_s + ' vs P2=' + player2.count.to_s + ''
+  var = game.who_is_winning(game.check_win?, player1.name, player2.name)
+ unless(var.nil?)
+  print var == "player1" ? "\n#{player1.name} (player1) wins" : "\n#{player2.name} (player2) wins"
+ else
+  print 'Nobody wins'
+ end
 
-  print 'Do you want rematch Yes or No'
-  match = gets.chomp
+player1.winner_counter if "player1" == var
+player2.winner_counter if "player2" == var
+
+puts "\n\n RESULTS= P1=" + player1.count.to_s + ' vs P2=' + player2.count.to_s + ''
+print 'Do you want rematch write Yes'
+
+match = gets.chomp
   if match.upcase == 'YES'
     rematch = true
     game.array = %w[a b c d e f g h i]
--- lib/game.rb
+++ lib/game.rb
@@ -6,55 +6,46 @@ class GameCondition
   def initialize(array = %w[a b c d e f g h i], count = 1)
     @array = array
     @count = count
+    @win_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
   end
 
   def self.convertsymbol(position)
     @hash.key?(position) ? @hash[position] : 'No Valid'
   end
 
-  def position_override(position, symbol)
+  def position_override?(position, symbol)
     if (@array[position] != 'X') && (@array[position] != 'O')
       @count += 1
       @array[position] = symbol
-      true
-    else
-      puts 'Position no available'
-      false
+      #true
+    #else
+      #false
     end
   end
 
-  def check_win
-    @win_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+  def check_win?
     @win_array.length.times do |i|
       comparison1 = @array[@win_array[i][1]] == @array[@win_array[i][0]]
       comparison2 = @array[@win_array[i][1]] == @array[@win_array[i][2]]
       return @array[@win_array[i][0]] if comparison1 && comparison2
     end
     return true if @count == 10
-
     false
   end
 
   def printboard
-    puts "__#{@array[0]}_|_#{@array[1]}_|_#{@array[2]}__"
-    puts "__#{@array[3]}_|_#{@array[4]}_|_#{@array[5]}__"
-    puts "  #{@array[6]} | #{@array[7]} | #{@array[8]}  "
+    "__#{@array[0]}_|_#{@array[1]}_|_#{@array[2]}__\n"+
+    "__#{@array[3]}_|_#{@array[4]}_|_#{@array[5]}__\n"+
+    "  #{@array[6]} | #{@array[7]} | #{@array[8]}\n"
   end
 
   def who_is_winning(symbol, player1, player2)
     if symbol == 'O'
-      print "#{player2} (Player 2) wins"
-      player2
+      "player2"
     elsif symbol == 'X'
-      print "#{player1} (Player 1) wins"
-      player1
+      "player1"
     else
-      print 'Nobody wins'
       nil
     end
-  end
-
-  def player_welcome(name, symbol)
-    puts "You're welcome" + ' ' + name + ' Your symbol is ' + symbol
   end
 end
